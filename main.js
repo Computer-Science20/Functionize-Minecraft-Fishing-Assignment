9dfe1272c93b2f44aa60d20f5443b3e074065a6b
dhiyashah <dhiyashah2005@gmail.com>
2022-07-07 11:39:37 -0600

    Update

--- main.js
+++ main.js
@@ -28,45 +28,12 @@ function fishBtnClicked() {
   // Catch fish based on character
   if (character === "steve") {
     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    let randNum = Math.random();
-    if (randNum < 0.7) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = "img/Raw-Cod.png";
-    } else if (randNum < 0.9) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = "img/Raw-Salmon.png";
-    } else if (randNum < 0.95) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = "img/Tropical-Fish.png";
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = "img/Pufferfish.png";
+    catchFish(number < 1, fishType = any, fishNum = any);
     }
-  } else if (character === "alex") {
+  else if (character === "alex") {
     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-    let randNum = Math.random();
-    if (randNum < 0.1) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = "img/Raw-Cod.png";
-    } else if (randNum < 0.2) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = "img/Raw-Salmon.png";
-    } else if (randNum < 0.5) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = "img/Tropical-Fish.png";
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = "img/Pufferfish.png";
-    }
-  }
+    catchFish(number < 1, fishType = any, fishNum = any); 
+}
 }
 
 // Event handler for resetBtn
@@ -80,4 +47,24 @@ function resetBtnClicked() {
   numSalmonSpan.innerHTML = numSalmon;
   numTropicalSpan.innerHTML = numTropical;
   numPufferSpan.innerHTML = numPuffer;
+}
+
+function catchFish(number, fishType, fishNum) {
+        if (number < 1) {
+          fishNum++;
+          fishNumSpan.innerHTML = fishNum;
+          resultImg.src = `img/${fishType}.png`;
+        } else if (number < 1) {
+          fishNum++;
+          fishNumSpan.innerHTML = fishNum;
+          resultImg.src = `img/${fishType}.png`;
+        } else if (number < 1) {
+          fishNum++;
+          fishNumSpan.innerHTML = fishNum;
+          resultImg.src = `img/${fishType}.png`;
+        } else {
+          fishNum++;
+          fishNumSpan.innerHTML = fishNum;
+          resultImg.src = `img/${fishType}.png`;
+        }
 }
